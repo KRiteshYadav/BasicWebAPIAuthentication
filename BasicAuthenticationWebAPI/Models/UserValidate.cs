fec52cc960d5c8f40efdd55d6b3f1c9a67df487a
Yadav <ky185041@ncr.com>
2020-03-23 16:39:48 +0530

    Added Files

---
+++ BasicAuthenticationWebAPI/Models/UserValidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicAuthenticationWebAPI.Models
+{
+    public class UserValidate
+    {
+        public static bool Login(string username , string password)
+        {
+            UserBL userBl = new UserBL();
+            var UserLists = userBl.GetUsers();
+            return UserLists.Any(user => user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase) && user
+            .Password == password);
+        }
+    }
+}
